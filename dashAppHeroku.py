f98ba363d5caede1e63192544f922fe8119655b4
frsegundo <62154248+frsegundo@users.noreply.github.com>
2020-10-27 10:10:09 -0300

    Atualização de setembro -3

--- dashAppHeroku.py
+++ dashAppHeroku.py
@@ -28,7 +28,7 @@ ICMS=pd.read_excel(enderecoAlterna,sheet_name="ICMS",index_col=0)
 IPVA=pd.read_excel(enderecoAlterna,sheet_name="IPVA",index_col=0)
 Rec173=pd.read_excel(enderecoAlterna,sheet_name="Recursos173",index_col=0)
 Sus173=pd.read_excel(enderecoAlterna,sheet_name="Suspensao173",index_col=0)
-nomeMeses=["janeiro","fevereiro","março","abril","maio","junho","julho","agosto"]
+nomeMeses=["janeiro","fevereiro","março","abril","maio","junho","julho","agosto","setembro"]
 beginner='TD' #estado que é plotado na abertura, se alterar aqui, alterar dentro de listaPorEstado
 benchSuficiencia=1.0 #barra de suficiência que sera benchmark para os indices que serao traçados
 textoArrecada='Não perdeu arrecadação.'
@@ -730,7 +730,7 @@ def update_output(value):
     bargap=0.15, # gap between bars of adjacent location coordinates.
     bargroupgap=0.1 # gap between bars of the same location coordinate.
   )
-  graficoSuf.update_yaxes(range=[80, 180]) # eixo y da figura customizado manualmente
+  graficoSuf.update_yaxes(range=[80, 200]) # eixo y da figura customizado manualmente
   return graficoSuf
 
 
@@ -758,7 +758,7 @@ def update_output(value):
         legend_orientation="h"
     )
   
-    graficoArrecadCap.update_yaxes(title_text="Diferença %", secondary_y=False)
+    graficoArrecadCap.update_yaxes(title_text="Diferença", secondary_y=False)
     return graficoArrecadCap
 
 #não esqueça desta linha para conseguir rodar sua aplicação
